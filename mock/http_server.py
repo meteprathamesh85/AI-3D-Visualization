9cff7fbd4c54eabac5e8f46fcb230634dc05cdb7
meteprathamesh85 <meteprathamesh85@gmail.com>
2026-09-06 19:11:18 +0530

    Prepare HTTP event backend for cloud deployment

--- mock/http_server.py
+++ mock/http_server.py
@@ -1,10 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from datetime import datetime, timezone
 import json
+import os
 
-
-HOST = "127.0.0.1"
-PORT = 8000
+HOST = "0.0.0.0"
+PORT = int(os.environ.get("PORT", "8000"))
 
 
 AGENTS = [
@@ -97,6 +97,8 @@ class EventHandler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Cache-Control", "no-store")
         self.send_header("Content-Length", str(len(body)))
         self.end_headers()
 
